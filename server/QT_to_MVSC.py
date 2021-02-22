95577f68442d910c2397c9092c2409ec91d5902a
Karl <karl.lautenschlger@ivv-dd.fraunhofer.de>
2021-02-22 07:55:03 +0100

    QT

--- server/QT_to_MVSC.py
+++ server/QT_to_MVSC.py
@@ -44,7 +44,7 @@ async def hello(websocket, path):
                 await websocket.send(msg)
 
 
-start_server = websockets.serve(hello, "localhost", 8765)
+start_server = websockets.serve(hello, "localhost", 8770)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
